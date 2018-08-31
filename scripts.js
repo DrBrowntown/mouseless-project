c6505a9c1cb49e96e2fd4dcc74e62a82a66e0aac
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-08-31 18:43:37 -0400

    feat: Change camera sound

--- scripts.js
+++ scripts.js
@@ -26,15 +26,9 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
-        // take pixels out
+
         pixels = ctx.getImageData(0, 0, width, height);
-        // mess with them
-        // pixels = redEffect(pixels);
-
-        
-
-        // pixels = greenScreen(pixels);
-        // put them back
+  
         ctx.putImageData(pixels, 0, 0);
 
     }, 16);
@@ -51,53 +45,6 @@ function takePhoto() {
     link.innerHTML = `<img src="${data}" alt="Handsome" />`;
     strip.insertBefore(link, strip.firstChild);
 }
-
-// function redEffect(pixels) {
-//     for(let i = 0; i < pixels.data.length; i+=4) {
-//         pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-//         pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-//         pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
-//     }
-//     return pixels;
-// }
-
-// function rgbSplit(pixels) {
-//     for(let i = 0; i < pixels.data.length; i+=4) {
-//         pixels.data[i - 150] = pixels.data[i + 0]; // RED
-//         pixels.data[i + 100] = pixels.data[i + 1]; // GREEN
-//         pixels.data[i -150] = pixels.data[i + 2]; // Blue
-
-//     }
-//     return pixels;
-// }
-
-// function greenScreen(pixels) {
-//   const levels = {};
-
-//   document.querySelectorAll('.rgb input').forEach((input) => {
-//     levels[input.name] = input.value;
-//   });
-
-//   for (i = 0; i < pixels.data.length; i = i + 4) {
-//     red = pixels.data[i + 0];
-//     green = pixels.data[i + 1];
-//     blue = pixels.data[i + 2];
-//     alpha = pixels.data[i + 3];
-
-//     if (red >= levels.rmin
-//     // If any of the pixels are in between the min and max values
-//       && green >= levels.gmin
-//       && blue >= levels.bmin
-//       && red <= levels.rmax
-//       && green <= levels.gmax
-//       && blue <= levels.bmax) {
-//       // take it out!
-//       pixels.data[i + 3] = 0;
-//     }
-//   }
-
-//   return pixels;
-// }
 
 getVideo();
 
@@ -128,11 +75,6 @@ const transcript = Array.from(e.results)
   if(transcript.includes('cheese')) {
 
     takePhoto();
-  }
-  if(transcript.includes('Ruby')) {
-    console.log('ruby');
-    pixels = rgbSplit(pixels);
-    return pixels;
   }
   
 console.log(transcript);
